ab1e332fa58d747fcd5721f954f730d208cad84d
Segniko <140623273+Segniko@users.noreply.github.com>
2024-09-25 09:25:23 +0300

    Update nutritionRoutes.js

--- server/routes/nutritionRoutes.js
+++ server/routes/nutritionRoutes.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const {
-    createNutritionEntry,
-    getAllNutritionEntries,
-} = require('../controllers/nutritionController');
+const { getAllNutrition, addNutrition, updateNutrition, deleteNutrition } = require('../controllers/nutritionController');
 
 const router = express.Router();
 
-// Route for fetching all nutrition data
-router.get('/', getAllNutritionEntries);
-
-// Route for creating a new nutrition entry
-router.post('/', createNutritionEntry); // POST route for creating nutrition entry
+// Define routes
+router.get('/', getAllNutrition);
+router.post('/', addNutrition);
+router.put('/:id', updateNutrition);
+router.delete('/:id', deleteNutrition);
 
 module.exports = router;
